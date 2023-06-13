cb45ca53ef0bdb10ae278792874938200f0ae69f
ESPodmov <90144811+ESPodmov@users.noreply.github.com>
2023-06-13 13:03:52 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = "5418373777:AAE6C9HjafZllmIU7YLU5Pf2nyivM9nBFiY"
-MONGOKEY = "mongodb+srv://CinemaAdmin:Nosorog20025555@multicinema.qag6irs.mongodb.net/?retryWrites=true&w=majority"
-MONGO_DB = "multicinema"
